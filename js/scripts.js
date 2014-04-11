516a29f9a5428a929e04bc51d3590849cc574278
Joseph Bergdoll <j@varsity.is>
2014-04-11 15:33:07 -0400

    header fades working

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 
+  $('.project-hero h1').waypoint(function(direction) {
+    $('.top-nav').toggleClass('pre-fixed');
+  });
+
   $('.buttons').waypoint(function(direction) {
-    $('.top-nav').toggleClass('fade');
+    $('.top-nav').toggleClass('addtransitions');
   });
 
   $('#about').waypoint(function(direction) {
